71961ba7b0fae8a1288e4afb9311d9747707369e
Jason Anton <rev.jt.anton@gmail.com>
2021-03-07 20:29:58 -0500

    doc: filled out more documentation and tests

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
+// index.ts
+/**
+ * A AWS Lambda helper package
+ * 
+ * @module
+ */
 import HttpStatusCode from './HttpStatusCode'
 import { APIGatewayProxyEvent } from 'aws-lambda'
 
@@ -12,19 +18,40 @@ export interface HttpResponse {
 }
 
 /**
- * Will return fully formatted and ready 
- * HTTP response for Lambda delivery
- * @param statusCode 
- * @param origin
- * @param format 
+ * Will return fully formatted and ready HTTP response for Lambda delivery
+ * 
+ * @param statusCode - An HTTP response code
+ * @param format - If you need to parse your body send the parser here
+ * 
+ * @example
+ * Sets a function to return a 200 OK response
+ * ```ts
+ * const ok = util.withStatusCode(200, JSON.stringify)
+ * const bad = util.withStatusCode(400)
+ * ```
+ * 
+ * @returns A function that can be called to send an HTTP response
  */
-const withStatusCode = (statusCode: number, origin: string, format?: Function): Function => {
+const withStatusCode = (statusCode: number, format?: Function): Function => {
   if (100 > statusCode || statusCode > 599) {
     throw new Error('status code out of range')
   }
 
-  // return a function that will take some data and formats a response with a status code
-  return (data: string | Record<string, unknown> | Array<any> | void): HttpResponse => {
+  /**
+   * The function that sends the HTTP response
+   * 
+   * @param data - The information you are sending
+   * @param origin - What domain can receive this response
+   * 
+   * @example
+   * Returns a JSON stringified var body to a localhost domain
+   * ```ts
+   * return ok(body, 'http://localhost')
+   * ```
+   * 
+   * @returns Formatted and parsed response
+   */
+  return (data: string | Record<string, unknown> | Array<any> | void, origin = '*'): HttpResponse => {
     const response: HttpResponse = {
       statusCode: statusCode,
       headers: {
@@ -44,8 +71,11 @@ const withStatusCode = (statusCode: number, origin: string, format?: Function): 
 }
 
 /**
+ * Ensuring the header exists in the API request and then parses it
  * 
- * @param apiGatewayProxyEvent 
+ * @param apiGatewayProxyEvent - The event coming from the API Gateway request
+ * 
+ * @returns The headers parsed into a Object
  */
 const validateAndParseRequestHeaders = (apiGatewayProxyEvent: APIGatewayProxyEvent): Record<string, unknown> | null => {
   if (apiGatewayProxyEvent !== null && apiGatewayProxyEvent.headers !== null && apiGatewayProxyEvent.headers !== undefined) {
@@ -58,8 +88,10 @@ const validateAndParseRequestHeaders = (apiGatewayProxyEvent: APIGatewayProxyEve
 }
 
 /**
+ * Ensuring the body eixists in the API request and then parses it
+ * @param apiGatewayProxyEvent - The event coming from the API Gateway request
  * 
- * @param apiGatewayProxyEvent 
+ * @returns The body parsed into an object 
  */
 const validateAndParseRequestBody = (apiGatewayProxyEvent: APIGatewayProxyEvent): string | null => {
   if (apiGatewayProxyEvent !== null && apiGatewayProxyEvent.body !== null && apiGatewayProxyEvent.body !== undefined) {
